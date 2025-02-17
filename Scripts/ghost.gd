d9e333bc46c934dc32b1e08c74ac5a41caa06429
Hassan <hassanmines@gmail.com>
2025-02-17 12:52:11 +0700

    Implementasi Jump Scare
    Ubah bebrapa fungsi agar game berjalan dengan baik

--- Scripts/ghost.gd
+++ Scripts/ghost.gd
@@ -4,8 +4,17 @@ extends GameCharacter
 static var instance: Ghost
 
 func _enter_tree() -> void:
+	if multiplayer.is_server():
+		$Area2D.body_entered.connect(check_for_player)
+	else:
+		$Area2D.queue_free()
 	super._enter_tree()
+	
 	print(current_character)
 	instance = self
 	if not current_character:
-		$Sprite.self_modulate.a = 0.25
+		%sprite.self_modulate.a = 0.25
+		
+func check_for_player(body: Node2D) -> void:
+	if body is Player:
+		body.stun.rpc()
--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -1,7 +1,30 @@
 class_name Player 
 extends GameCharacter
-
+var stun_time_left:float
+@onready var normal_movement_speed: float = movement_speed
 static var instance: Player
 
 func _ready():
 	instance = self
+	
+func _physics_process(delta: float) -> void:
+	super._physics_process(delta)
+	if multiplayer.is_server():
+		if stun_time_left > 0.0:
+			stun_time_left -= delta
+			if stun_time_left <= 0.0:
+				destun.rpc()
+				
+@rpc("call_local")
+func stun() -> void:
+	movement_speed=normal_movement_speed*0.375
+	if multiplayer.is_server():
+		stun_time_left= 3.0
+	if current_character:
+		GameManager.instance.hud.add_child(load("res://Scenes/jumpscare.tscn").instantiate())
+		
+@rpc("call_local")
+func destun()->void:
+	movement_speed=normal_movement_speed
+		
+		
