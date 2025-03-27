59d4ac451acbc5c386d75101cf89b9c075345101
Hassan <hassanmines@gmail.com>
2025-03-28 00:47:16 +0700

    made Api Unggun and added
    Burning sfx too

--- Scripts/trash_bin.gd
+++ Scripts/trash_bin.gd
@@ -11,4 +11,7 @@ func _on_body_entered(body: Node2D) -> void:
 
 @rpc("authority", "call_local")
 func remove_trash(trash_path: NodePath) -> void:
+	var fire_effect: Node2D = preload("res://Scenes/cpu_particles_2d.tscn").instantiate()
+	fire_effect.global_position = $Marker2D.global_position
+	add_sibling(fire_effect)
 	get_node(trash_path).queue_free()
